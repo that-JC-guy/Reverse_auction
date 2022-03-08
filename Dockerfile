3aa7e47135508b76e1741615db2684750dfe5839
that-jc-guy <thatjcguy@live.com>
2022-03-08 09:10:36 -0500

    updated HTML doc

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ COPY . .
 
 EXPOSE 5000
 
-CMD [ "node", "index.js" ]
+CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const fs = require('fs')
-const port = 3000
+const port = 5000
 
 const server = http.createServer(function(req, res){
     res.writeHead(200, { 'Content-Type': 'text/html'})
