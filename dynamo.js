40d90f1cd8cb03771aab7f043be9e878557461ed
that-jc-guy <thatjcguy@live.com>
2022-03-09 15:38:21 -0500

    fixed getting list of bids

--- dynamo.js
+++ dynamo.js
@@ -17,10 +17,14 @@ const getBids = async () => {
     };
     const bids = await dynamoClient.scan(params).promise();
     //console.log(bids);
+    //console.log(bids.$response.data.Count)
+
+    //let processedBids = [bids.$response.data.Items]
+    //var test = processedBids[0][0].city
+    //console.log(test)
+
     return bids;
 }
-
-var allBids = getBids();
 
 async function addUpdateBid(bid) {
     const params = {
@@ -30,22 +34,46 @@ async function addUpdateBid(bid) {
     return await dynamoClient.put(params).promise();
 }
 
+function displayBids(bids) {
+    let processedBids = [bids.$response.data.Items]
+    allBids = []
+    for (i = 0; i <= processedBids.length; i++) {
+        allBids[i] = processedBids[0][i]["bidAmount"]
+    }
+    console.log(allBids)
+}
+
+async function init() {
+    var allBids = await getBids()
+    displayBids(allBids)
+}
+
+init ()
+
 module.exports = {
     dynamoClient,
     getBids,
     addUpdateBid
 }
 
-/*const bid = {
-    "bidId": 1,
-    "bidAmount": 990,
-    "name": "Joe Test",
-    "streetAddress": "99 Here St",
-    "city": "Springfield",
-    "state": "IL",
-    "zipcode": "15251",
-    "tokenCount": 5
+/* // Used for adding new table entries for testing only
+const bidderId = parseInt(Math.floor(Math.random()*10000)+1);
+const bidId = parseInt(Math.floor(Math.random()*1000)+1);
+
+const bid = {
+"auctionId": "2Q2022-TEST",
+"bidderId": bidderId,
+"bidId": bidId,
+"bidAmount": 990,
+"name": "John Q. Tokenholder",
+"streetAddress": "123 Main St",
+"city": "Manchester",
+"state": "NH",
+"zipcode": "03102",
+"tokenCount": 5,
 }
+
+
 
 addUpdateBid(bid);
 */
