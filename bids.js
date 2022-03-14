ab49a6642a63e9ecc0e8650ed81ceefcfaf0c598
that-JC-guy <35340390+that-JC-guy@users.noreply.github.com>
2022-03-14 07:11:10 -0400

    setup query filter

--- bids.js
+++ bids.js
@@ -1,24 +1,48 @@
 // Functions related to bid management AFTER retrieval from DB source.
-const { getBids, addUpdateBid } = require('./dynamo');
+const { getBids } = require('./dynamo');
 
 function getLowestBid(bids) {
-    console.log(bids)
-    //let processedBids = [bids.$response.data.Items]
-    // processedBids = bids
-    // allBids = []
-    // for (i = 0; i <= processedBids.length; i++) {
-    //     allBids[i] = processedBids[0][i]["bidAmount"]
-    // }
-    // const lowestBid = Math.min(...allBids)
-    // return lowestBid
+    let processedBids = [bids.$response.data.Items]
+
+    allBids = []
+    for (i = 0; i < bids.$response.data.Count; i++) {
+        allBids[i] = processedBids[0][i]["bidAmount"]
+        //console.log('Bid Entry: ' + processedBids[0][i]["bidAmount"])
+    }
+    const lowestBid = Math.min(...allBids)
+    return lowestBid
+}
+
+function sortBids(bids){
+    let allBids = bids.$response.data.Items
+    const sortedBids = allBids.sort(function(a,b){return a.bidAmount - b.bidAmount})
+
+    return sortedBids
 }
 
 function getTopTenBids(bids) {
-    let processedBids = [bids.$response.data.Items]
+    let processedBids = bids.$response.data.Items
+
+    // load the relevant bid data into new object
     allBids = []
-    for (i = 0; i <= 10; i++) {
-        allBids[i] = processedBids[0][i]["bidAmount", "slpAddress"]
+    for (i = 0; i < bids.$response.data.Count; i++) {
+        const bidPair = {
+            'bidAmount:': processedBids[i]['bidAmount'],  
+            'slpAddress': processedBids[i]['slpAddress'] 
+        }
+        allBids.push(bidPair)
     }
+
+    //sort all bids by bidAmount in ascending order
+    let sortedBids = allBids.reverse(function(a,b){return a.bidAmount - b.bidAmount})
+
+    //return the top ten bids
+    topTenBids = []
+    for (i = 0; i < 10; i++){
+        topTenBids[i] = sortedBids[i]
+    }
+
+    return topTenBids
 }
 
 module.exports = {
